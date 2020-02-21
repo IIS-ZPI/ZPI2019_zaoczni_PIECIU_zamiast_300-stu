3436a8193d2dd936cd2a54418243e3c3a3390987
arson_dev <40763415+arsonDev@users.noreply.github.com>
2020-02-21 23:58:46 +0100

    Poprawa wyswietlania wykresów

--- app/src/main/java/pl/arsonproject/nbp_currency/ui/utils/Utils.kt
+++ app/src/main/java/pl/arsonproject/nbp_currency/ui/utils/Utils.kt
@@ -1,2 +1,9 @@
 package pl.arsonproject.nbp_currency.ui.utils
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun LocalDate.getDateString() = this.format(DateTimeFormatter.ofPattern("YYYY-MM-dd")).toString()
